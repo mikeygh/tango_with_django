7df4b605047b7fa903eaeb7ba4b439674b3f46d6
mikey <michael.kambli@gmail.com>
2016-02-19 10:42:29 -0800

    Adding  new About view

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says hey there world!  <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'>Index</a> ")
